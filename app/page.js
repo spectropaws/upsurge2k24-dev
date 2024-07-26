644ca2b18bc9e8b372fc6780ba5ba9f7e4d8ae73
spectropaws <spectropaws777@gmail.com>
2024-07-26 08:11:53 +0530

    initialize next js and shadcn

---
+++ app/page.js
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <Link href="/smackathon" className="text-blue-800 underline text-2xl">Smackathon page</Link>
+      );
+}

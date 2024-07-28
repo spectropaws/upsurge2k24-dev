e8abb0354aac9a9e95ca99962fe768de670da65b
laukik-24 <laukiklanje24@gmail.com>
2024-07-28 22:39:08 +0530

    updated css

--- app/smackathon/page.js
+++ app/smackathon/page.js
@@ -1,5 +1,13 @@
+import Footer from "../../components/footer/footer";
+
 export default function Smackathon() {
-    return <div className="text-xl flex justify-center items-center h-screen text-5xl">
+  return (
+    <>
+      <div className="text-xl flex justify-center items-center h-screen ">
         <h1>Smackathon</h1>
-    </div>;
+      </div>
+
+      <Footer />
+    </>
+  );
 }
---
+++ components/footer/footer.js
@@ -0,0 +1,9 @@
+import styles from "../../styles/footer.module.css";
+
+export default function footer() {
+  return (
+    <div className={styles.footer}>
+      <p>hello</p>
+    </div>
+  );
+}

e8cec87c99ec019c44e248a09579609462c83106
supremeStrange-04 <supreme.strange.199@gmail.com>
2024-08-01 20:00:04 +0530

    Prize section and Goodies Section added along with images/prize and images/goodies

---
+++ app/smackathon/components/Goodies.jsx
@@ -0,0 +1,48 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+
+export default function Goodies() {
+  return (
+    <div className="relative bg-[url('/images/goodies/gfg-final.png')] w-full bg-cover bg-center py-20">
+      <h1 className="text-4xl p-6 pb-12 text-center text-white font-bold">
+        Exclusive Goodies from GFG
+      </h1>
+      <div className="flex flex-col gap-5 items-center px-16 pb-40 lg:flex-row">
+        <Image
+          src="/images/goodies/goodies.png"
+          width={378}
+          height={403}
+          className="bg-[radial-gradient(circle,_rgba(255,_215,_0,_0.7),_rgba(255,_215,_0,_0)_70%)] p-6  rounded-lg shadow-lg ml-0 lg:ml-32"
+        />
+        <div className="flex flex-col gap-8 ">
+          <div className="flex items-center ">
+            <Image src="/images/goodies/heart.png" width={50} height={50} className="lg:mt-1"/>
+            <span className="text-lg text-white lg:text-xl">
+              GFG Kits to the top 3 winners of the Hackathon.
+            </span>
+          </div>
+          <div className="flex items-center ">
+            <Image src="/images/goodies/heart.png" width={50} height={50} />
+            <span className="text-lg text-white lg:text-xl">
+              GFG Discount coupons worth Rs 2000 for winners.
+            </span>
+          </div>
+          <div className="flex items-center ">
+            <Image src="/images/goodies/heart.png" width={50} height={50} />
+            <span className="text-lg text-white lg:text-xl">
+              GFG Discount coupons worth Rs 300 for all participants.
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <img
+        src="/images/goodies/character.png"
+        className="absolute bottom-0 left-0 h-56"
+        alt="Character"
+      />
+    </div>
+  );
+
+}
---
+++ app/smackathon/components/PrizeSection.jsx
@@ -0,0 +1,186 @@
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+export default function Prize() {
+  return (
+    <>
+      <Image
+        src="/images/prize/temp1.png"
+        width={2020}
+        height={734}
+        className="w-full bg-cover"
+      />
+      {/* <img src= className="" /> */}
+      <div className="bg-[url('/images/prize/prize-bg.png')] w-full bg-cover flex flex-col items-center">
+        <h2 className="text-5xl mt-16 text-center text-white font-bold">
+          Cash Prizes
+        </h2>
+        <div className="flex flex-col gap-20 lg:flex-row px-24 py-32">
+          <div className="flex flex-col items-center gap-10 order-1 md:order-0">
+            <span className="font-semibold text-white text-xl">Prize 2</span>
+
+            <div className="bg-[radial-gradient(circle,_rgba(147,_133,_227,_0.6),_rgba(147,_133,_227,_0)_75%)] p-14">
+              <motion.div
+                animate={{
+                  y: [10, -10],
+                  transition: {
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                    duration: 1,
+                    // delay: 1,
+                  },
+                }}
+                whileHover={{ scale: 1.1 }}
+              >
+                <Image src="/images/prize/Prize_2.png" width={338} height={397}/>
+              </motion.div>
+            </div>
+            <span className="font-medium text-white text-xl">5000</span>
+          </div>
+          <div className="flex flex-col items-center gap-10 order:0 md:order-1">
+            <span className="font-semibold text-white text-xl">Prize 1</span>
+            <div className="bg-[radial-gradient(circle,_rgba(147,_133,_227,_0.6),_rgba(147,_133,_227,_0)_75%)] p-14">
+              <motion.div
+                animate={{
+                  y: [10, -10],
+                  transition: {
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                    duration: 1,
+                    delay: 1,
+                  },
+                }}
+                whileHover={{ scale: 1.1 }}
+              >
+                <Image src="/images/prize/Prize_1.png" width={430} height={442} />
+              </motion.div>
+            </div>
+
+            <span className="font-medium text-white text-xl">10000</span>
+          </div>
+          <div className="flex flex-col items-center gap-10 order-1">
+            <span className="font-semibold text-white text-xl">Prize 3</span>
+
+            <div className="bg-[radial-gradient(circle,_rgba(147,_133,_227,_0.6),_rgba(147,_133,_227,_0)_75%)] p-16">
+              <motion.div
+                animate={{
+                  y: [10, -10],
+                  transition: {
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                    duration: 1,
+                    // delay: 1,
+                  },
+                }}
+                whileHover={{ scale: 1.1 }}
+              >
+                <Image src="/images/prize/Prize_3.png" width={338} height={397} />
+              </motion.div>
+            </div>
+
+            <span className="font-medium text-white text-xl">2000</span>
+          </div>
+        </div>
+      </div>
+      <Image src="/images/prize/transition-nether-new.png" width={1440} height={348} className="w-full bg-cover" />
+    </>
+  );
+}
+
+// "use client";
+// import React, { useState, useEffect } from "react";
+// import { motion } from "framer-motion";
+// import Image from "next/image";
+
+// export default function Prize() {
+//   const width = useState(window.innerWidth);
+//   const images = [
+//     "/images/prize/ores.png",
+//     "/images/prize/diamond.png",
+//     "/images/prize/gold.png",
+//   ];
+
+//   return (<>
+//     <img src="/images/prize/temp4.png" alt="" />
+//     <div className="bg-[url('/images/prize/cave3.avif')] w-full bg-cover flex flex-col items-center">
+//       <h2 className="text-5xl mt-16 text-center text-white font-bold">
+//         Cash Prizes
+//       </h2>
+//       <div className="flex flex-col gap-40 lg:flex-row py-32">
+//         <div className="flex flex-col items-center gap-10 order-1 md:order-0">
+//           <span className="font-semibold text-white text-xl">Prize 2</span>
+
+//           <div className="bg-[radial-gradient(circle,_rgba(255,_215,_0,_0.6),_rgba(255,_215,_0,_0)_75%)] p-14">
+//             <motion.div
+//               animate={{
+//                 y: [10, -10],
+//                 transition: {
+//                   repeat: Infinity,
+//                   repeatType: "reverse",
+//                   duration: 1,
+//                   // delay: 1,
+//                 },
+//               }}
+//               whileHover={{ scale: 1.1 }}
+//             >
+//              <Image
+//                 width={200}
+//                 height={200}
+//                 src={width < 700 ? images[0] : images[1]}
+//               />
+//             </motion.div>
+//           </div>
+//           <span className="font-medium text-white text-xl">5000</span>
+//         </div>
+//         <div className="flex flex-col items-center gap-10 order:0 md:order-1">
+//           <span className="font-semibold text-white text-xl">Prize 1</span>
+//           <div className="bg-[radial-gradient(circle,_rgba(255,_215,_0,_0.6),_rgba(255,_215,_0,_0)_75%)] p-14">
+//             <motion.div
+//               animate={{
+//                 y: [10, -10],
+//                 transition: {
+//                   repeat: Infinity,
+//                   repeatType: "reverse",
+//                   duration: 1,
+//                   delay: 1,
+//                 },
+//               }}
+//               whileHover={{ scale: 1.1 }}
+//             >
+//               <Image
+//                 width={300}
+//                 height={300}
+//                 src={width < 700 ? images[1] : images[0]}
+//               />
+//             </motion.div>
+//           </div>
+
+//           <span className="font-medium text-white text-xl">10000</span>
+//         </div>
+//         <div className="flex flex-col items-center gap-10 order-1">
+//           <span className="font-semibold text-white text-xl">Prize 3</span>
+
+//           <div className="bg-[radial-gradient(circle,_rgba(255,_215,_0,_0.6),_rgba(255,_215,_0,_0)_75%)] p-16">
+//             <motion.div
+//               animate={{
+//                 y: [10, -10],
+//                 transition: {
+//                   repeat: Infinity,
+//                   repeatType: "reverse",
+//                   duration: 1,
+//                   // delay: 1,
+//                 },
+//               }}
+//               whileHover={{ scale: 1.1 }}
+//             >
+//               <Image width={200} height={200} src={images[2]} />
+//             </motion.div>
+//           </div>
+
+//           <span className="font-medium text-white text-xl">2000</span>
+//         </div>
+//       </div>
+//     </div></>
+//   );
+// }

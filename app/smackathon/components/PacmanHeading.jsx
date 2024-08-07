60b18475325bc44496063f6fc9f0f6d046321314
mehul515 <mehulkohad@gmail.com>
2024-08-07 20:32:25 +0530

    update heading of all components and fix mobile view of pacman component

--- app/smackathon/components/PacmanHeading.jsx
+++ app/smackathon/components/PacmanHeading.jsx
@@ -21,12 +21,12 @@ export default function PacmanHeading() {
                 </div>
 
                 <div className={`${style.textLine}`}>
-                    <h1 className={`${style.p} ${style.h1}`}>
+                    <div className={`${style.p} ${style.h1}`}>
                         P
                         <div className={`${style.pacMan}`}>
                             <div className={`${style.eye}`}></div>
                         </div>
-                    </h1>
+                    </div>
 
                     <div className={`${style.awesome}`}>
                         <div className={`${style.awesomeBoard}`}>
@@ -53,22 +53,22 @@ export default function PacmanHeading() {
                             </div>
                         </div>
                     </div>
-                    <h1 className={`${style.c} ${style.h1}`}>
+                    <div className={`${style.c} ${style.h1}`}>
                         c
                         <div className={`${style.pacMan}`}>
                             <div className={`${style.eye}`}></div>
                         </div>
-                    </h1>
+                    </div>
                 </div>
-                <h1 className={`${style.dash}`}>-</h1>
+                <div className={`${style.dash}`}>-</div>
                 <div className={`${style.textLine}`}>
-                    <h1 className={`${style.m}  ${style.h1}`}>
+                    <div className={`${style.m}  ${style.h1}`}>
                         M
                         <div className={`${style.ghost} ${style.ghost2}`}>
                             <div className={`${style.eye}`}></div>
                             <div className={`${style.eye}`}></div>
                         </div>
-                    </h1>
+                    </div>
                     <div className={`${style.awesome}`}>
                         <div className={`${style.awesomeBoard}`}>
                             <div className={`${style.dot}`}></div>
@@ -92,13 +92,13 @@ export default function PacmanHeading() {
                             </div>
                         </div>
                     </div>
-                    <h1 className={`${style.n} ${style.h1}`}>
+                    <div className={`${style.n} ${style.h1}`}>
                         n
                         <div className={`${style.ghost} ${style.ghost2}`}>
                             <div className={`${style.eye}`}></div>
                             <div className={`${style.eye}`}></div>
                         </div>
-                    </h1>
+                    </div>
                 </div>
             </div>
         </div>

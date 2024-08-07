cd78c406d3d0f2217c484e4dba2cab4f78a02889
supremeStrange-04 <supreme.strange.199@gmail.com>
2024-08-07 13:49:02 +0530

    heading fixes

--- app/smackathon/PrizeSection.jsx
+++ app/smackathon/PrizeSection.jsx
@@ -7,12 +7,7 @@ export default function Prize() {
   return (
     <>
       <div className="relative">
-        <Image
-          src="/svgs/prize/ground.svg"
-          width={2020}
-          height={734}
-          className="w-full object-cover"
-        />
+        <div className="bg-[url(/svgs/prize/ground.svg)] bg-cover h-[60vh]"></div>
         <div className="flex absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] ">
           <button className={styles.btnfinale}>Download Brochure</button>
           {/* <button class={styles.btndb}>
@@ -29,9 +24,9 @@ export default function Prize() {
         className="bg-[url('/images/prize/new-prize-bg.png')] w-full bg-center flex flex-col items-center"
       >
 
-        <h2 className="text-5xl mt-16 text-center text-white font-bold">
+        <h1 className="text-5xl mt-16 text-center text-white font-bold">
           Cash Prizes
-        </h2>
+        </h1>
         <div className="flex flex-col gap-8 px-12 lg:flex-row lg:px-24 lg:gap-20 py-32 md:px-24 md:gap-20">
           <div className="flex flex-col items-center gap-10 order-1 md:order-0">
             <div className="p-14 bg-[radial-gradient(circle,_rgba(147,_133,_227,_0.4),_rgba(147,_133,_227,_0)_72%)] lg:bg-[radial-gradient(circle,_rgba(147,_133,_227,_0.6),_rgba(147,_133,_227,_0)_75%)]">

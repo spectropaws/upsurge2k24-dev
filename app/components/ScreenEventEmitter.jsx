047ea2aafd19bb98356e4c6c101b5e5a0ce20e9a
spectropaws <spectropaws777@gmail.com>
2024-08-08 22:12:32 +0530

    fixed build error

--- app/components/ScreenEventEmitter.jsx
+++ app/components/ScreenEventEmitter.jsx
@@ -1,3 +1,7 @@
+"use client";
+
+import React, { useEffect } from "react";
+
 const handleScreenClick = (e) => {
     e.stopPropagation();
     window.parent.postMessage('screenClick', '*');
@@ -10,9 +14,21 @@ const handleKeyDown = (e) => {
     }
 }
 
-window.addEventListener("click", handleScreenClick);
-window.addEventListener("keydown", handleKeyDown);
-
 export default function ScreenEventEmitter({ children }) {
+    useEffect(() => {
+        // Check if the window object is available
+        if (typeof window !== 'undefined') {
+            // Add event listeners
+            window.addEventListener("click", handleScreenClick);
+            window.addEventListener("keydown", handleKeyDown);
+            
+            // Clean up the event listeners on component unmount
+            return () => {
+                window.removeEventListener("click", handleScreenClick);
+                window.removeEventListener("keydown", handleKeyDown);
+            };
+        }
+    }, []);
+
     return <div className="w-full h-full m-0 p-0">{children}</div>
 }

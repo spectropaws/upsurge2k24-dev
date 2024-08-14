8fafbad2e00e5393679bad67e2cce60925152056
mehul515 <mehulkohad@gmail.com>
2024-08-14 15:50:31 +0530

    update sponsor scrollbar

--- app/home/components/Sponsors.jsx
+++ app/home/components/Sponsors.jsx
@@ -2,101 +2,141 @@
 import React from 'react';
 import Image from 'next/image';
 import StarsBackground from './StarsBackground';
+import { motion } from 'framer-motion';
 
 const Sponsors = () => {
-    const assessmentCriteria = [
+    const sponsorData = [
         {
             src: "/images/sponsors/webgurukul.png",
-            alt: "Quality of work submitted",
+            alt: "Webgurukul",
             width: "250"
         },
         {
             src: "/images/sponsors/cojag.png",
-            alt: "Timeliness of submissions",
-            width: "250"
-        },
-        {
-            src: "/images/sponsors/Gfg+White.png",
-            alt: "Participation in discussions",
+            alt: "cojag",
             width: "250"
         },
         {
             src: "/images/sponsors/new-Mk.png",
-            alt: "Innovation through imagination",
+            alt: "MK",
             width: "200"
         },
         {
             src: "/images/sponsors/unstop-logo.svg",
-            alt: "Innovation through imagination",
+            alt: "unstop",
             width: "250"
         },
         {
-            src: "/images/sponsors/webgurukul.png",
-            alt: "Quality of work submitted",
-            width: "250"
-        },
-        {
-            src: "/images/sponsors/cojag.png",
-            alt: "Timeliness of submissions",
-            width: "250"
-        },
-        {
-            src: "/images/sponsors/Gfg+White.png",
-            alt: "Participation in discussions",
-            width: "250"
-        },
-        {
-            src: "/images/sponsors/new-Mk.png",
-            alt: "Innovation through imagination",
+            src: "/images/sponsors/DBMart.png",
+            alt: "DB Mart",
             width: "200"
         },
         {
-            src: "/images/sponsors/unstop-logo.svg",
-            alt: "Innovation through imagination",
-            width: "250"
+            src: "/images/sponsors/times.png",
+            alt: "TIMES",
+            width: "200"
         },
-        
+        {
+            src: "/images/sponsors/brLifeStyle.png",
+            alt: "BR Life Styles",
+            width: "200"
+        },
     ];
 
     return (
         <StarsBackground>
-            <div className='pt-64'>
-                <h1 className='text-white text-center text-3xl md:text-5xl mb-10'>Sponsors</h1>
-                <div className='relative overflow-hidden'>
-                    <div className='flex animate-scroll'>
-                        {/* Duplicate the content twice to ensure seamless looping */}
-                        {assessmentCriteria.concat(assessmentCriteria).map((slide, i) => (
+            <div className="relative h-[100vh] pb-48">
+                {/* Large Planet Image */}
+                <div
+                    className=" hidden md:block absolute -top-[400px] -left-[200px] w-[600px] h-[600px] -z-10"
+                >
+                    <Image
+                        src="/images/sponsors/bluePlanet.png"
+                        alt="Large Planet"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+
+                <div
+                    className="absolute -top-40 md:-top-10 right-[200px] h-40 w-40 md:w-56 md:h-56 -z-10"
+                >
+                    <Image
+                        src="/images/sponsors/planet3.png"
+                        alt="Large Planet"
+                        layout="fill"
+                        objectFit="contain"
+                    />
+                </div>
+
+                {/* Glowing Enemy with Bounce Animation */}
+                <motion.div
+                    className="absolute right-[10%] top-[7%] w-[70px] h-[70px] md:top-[80px] md:left-[10%] md:w-[200px] md:h-[200px] -z-10"
+                    animate={{
+                        y: [0, '-7.5%', 0], // Move up and back down
+                    }}
+                    transition={{
+                        duration: 1.5, // Duration of one bounce cycle
+                        repeat: Infinity, // Repeat infinitely
+                        repeatType: "loop", // Loop the animation
+                        ease: "easeInOut", // Smooth transition
+                    }}
+                >
+                    <Image
+                        src="/images/sponsors/glowingEnemy.png"
+                        alt="Glowing Enemy"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </motion.div>
+
+                {/* Sponsors content */}
+                <div className="relative z-10 h-full">
+                    <h1 className="text-white text-center text-3xl md:text-5xl mb-10 mt-48">Sponsors</h1>
+                    <div className='flex h-full justify-center items-center'>
+                        <div className="relative w-[90%] overflow-hidden">
                             <div
-                                key={i}
-                                className='flex-shrink-0 flex justify-center items-center w-[250px] h-[250px] mx-[100px] md:w-[200px] rounded-lg'
+                                style={{
+                                    position: 'absolute',
+                                    inset: '0',
+                                    backgroundImage: 'linear-gradient(to right, #090A0F, white, #090A0F)',
+                                    opacity: '0.15',
+                                    borderRadius: '0'
+                                }}
+                            ></div>
+                            <motion.div
+                                className="flex"
+                                animate={{
+                                    x: ["0%", "-100%"], // Scroll left and loop back
+                                }}
+                                transition={{
+                                    duration: 30,
+                                    repeat: Infinity,
+                                    ease: "linear",
+                                }}
+                                style={{
+                                    display: "flex",
+                                    width: `calc(450px * ${sponsorData.length})`,
+                                }}
                             >
-                                <Image
-                                    src={slide.src}
-                                    alt={slide.alt}
-                                    width={slide.width}
-                                    height={250}
-                                    className='rounded-lg'
-                                />
-                            </div>
-                        ))}
+                                {sponsorData.concat(sponsorData).map((slide, i) => (
+                                    <div
+                                        key={i}
+                                        className="flex-shrink-0 flex justify-center items-center w-[300px] h-[300px] mx-[70px] rounded-lg relative"
+                                    >
+                                        <Image
+                                            src={slide.src}
+                                            alt={slide.alt}
+                                            width={slide.width}
+                                            height={250}
+                                            className="rounded-lg"
+                                        />
+                                    </div>
+                                ))}
+                            </motion.div>
+                        </div>
                     </div>
                 </div>
-                <style jsx>{`
-                    @keyframes scroll {
-                        0% {
-                            transform: translateX(0);
-                        }
-                        100% {
-                            transform: translateX(-100%);
-                        }
-                    }
-
-                    .animate-scroll {
-                        animation: scroll 30s linear infinite;
-                        display: flex;
-                        width: calc(450px * 8.9); /* 5 items, duplicated */
-                    }
-                `}</style>
             </div>
         </StarsBackground>
     );

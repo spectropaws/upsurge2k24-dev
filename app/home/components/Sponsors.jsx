c84ad5b9825ea4d7e81b6d52ee865036572a3e11
mehul515 <mehulkohad@gmail.com>
2024-08-15 11:36:03 +0530

    add news sponsors

--- app/home/components/Sponsors.jsx
+++ app/home/components/Sponsors.jsx
@@ -7,38 +7,48 @@ import { motion } from 'framer-motion';
 const Sponsors = () => {
     const sponsorData = [
         {
-            src: "/images/sponsors/webgurukul.png",
-            alt: "Webgurukul",
-            width: "250"
+            src: "/images/sponsors/gitConsultancy.png",
+            alt: "Git Consultancy",
+            width: "200"
         },
         {
             src: "/images/sponsors/cojag.png",
             alt: "cojag",
-            width: "250"
+            width: "220"
         },
         {
             src: "/images/sponsors/new-Mk.png",
             alt: "MK",
-            width: "200"
+            width: "150"
         },
         {
             src: "/images/sponsors/unstop-logo.svg",
             alt: "unstop",
-            width: "250"
+            width: "200"
         },
         {
             src: "/images/sponsors/DBMart.png",
             alt: "DB Mart",
-            width: "200"
+            width: "180"
         },
         {
             src: "/images/sponsors/times.png",
             alt: "TIMES",
-            width: "200"
+            width: "180"
         },
         {
             src: "/images/sponsors/brLifeStyle.png",
             alt: "BR Life Styles",
+            width: "180"
+        },
+        {
+            src: "/images/sponsors/dahiya.png",
+            alt: "Dahiya Momos",
+            width: "180"
+        },
+        {
+            src: "/images/sponsors/travosa.png",
+            alt: "Travosa",
             width: "200"
         },
     ];
@@ -48,7 +58,7 @@ const Sponsors = () => {
             <div className="relative h-[100vh] pb-48">
                 {/* Large Planet Image */}
                 <div
-                    className=" hidden md:block absolute -top-[400px] -left-[200px] w-[600px] h-[600px] -z-10"
+                    className=" hidden md:block absolute -top-[190px] -left-[150px] w-[380px] h-[380px] -z-10"
                 >
                     <Image
                         src="/images/sponsors/bluePlanet.png"
@@ -59,7 +69,7 @@ const Sponsors = () => {
                 </div>
 
                 <div
-                    className="absolute -top-40 md:-top-10 right-[200px] h-40 w-40 md:w-56 md:h-56 -z-10"
+                    className="absolute -top-40 md:-top-10 right-[200px] h-32 w-32 md:w-56 md:h-56 -z-10"
                 >
                     <Image
                         src="/images/sponsors/planet3.png"
@@ -71,7 +81,7 @@ const Sponsors = () => {
 
                 {/* Glowing Enemy with Bounce Animation */}
                 <motion.div
-                    className="absolute right-[10%] top-[7%] w-[70px] h-[70px] md:top-[80px] md:left-[10%] md:w-[200px] md:h-[200px] -z-10"
+                    className="absolute right-[10%] top-[7%] w-[70px] h-[70px] md:top-[80px] md:left-[8%] md:w-[120px] md:h-[120px] -z-10"
                     animate={{
                         y: [0, '-7.5%', 0], // Move up and back down
                     }}
@@ -92,7 +102,7 @@ const Sponsors = () => {
 
                 {/* Sponsors content */}
                 <div className="relative z-10 h-full">
-                    <h1 className="text-white text-center text-3xl md:text-5xl mb-10 mt-48">Sponsors</h1>
+                    <h1 className="text-white text-center text-3xl md:text-5xl md:mb-10 mt-48">Sponsors</h1>
                     <div className='flex h-full justify-center items-center'>
                         <div className="relative w-[90%] overflow-hidden">
                             <div
@@ -116,13 +126,13 @@ const Sponsors = () => {
                                 }}
                                 style={{
                                     display: "flex",
-                                    width: `calc(450px * ${sponsorData.length})`,
+                                    width: `calc(390px * 8.9)`,
                                 }}
                             >
                                 {sponsorData.concat(sponsorData).map((slide, i) => (
                                     <div
                                         key={i}
-                                        className="flex-shrink-0 flex justify-center items-center bg-white w-[300px] h-[300px] mx-[70px] rounded-xl relative"
+                                        className="flex-shrink-0 flex justify-center items-center bg-white w-[250px] h-[250px] mx-[70px] rounded-xl relative"
                                     >
                                         <Image
                                             src={slide.src}

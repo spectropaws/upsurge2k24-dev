3070dbd364e115bc051c8d7b5e47ab17251284cb
mehul515 <mehulkohad@gmail.com>
2024-08-15 22:45:38 +0530

    update events content and images.

--- app/home/components/events/eventlist.js
+++ app/home/components/events/eventlist.js
@@ -1,68 +1,50 @@
 const events = [
     {
-        title: 'Coding Casino',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?',
-        image: '/images/events/codingRelay.jpg'
+        title: 'Flashmob',
+        description: "Get ready for an explosive burst of energy at the CSE Department's FLASHMOB on August 23rd, 2024, at 3 PM! Join us in front of the CSE Department for spontaneous dance moves, electrifying music, and non-stop excitement!",
+        image: 'https://upsurge2k24.s3.ap-south-1.amazonaws.com/events/flashmob.png'
     },
-
     {
-        title: 'Cosmo Town',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?',
-        image: '/images/events/cosmoTown.jpg'
+        title: 'Cosmotown',
+        description: 'Cosmotown is a thrilling game mela with over 10 exciting minigames. Play, earn credits, and bid on fantastic prizes. Join us on August 24th from 10 AM to 3:30 PM, with an auction at 4 PM!',
+        image: 'https://upsurge2k24.s3.ap-south-1.amazonaws.com/events/cosmoTown.png'
     },
     {
         title: 'Desi Daredevils',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?',
-        image: '/images/events/desiDaredevils.png'
+        description: '"Desi Daredevils" offers an exhilarating three-stage game, designed for teams of four. Bring along your best friends as you explore the arena, enjoy mouthwatering food, and take on thrilling challenges.join us on August 23rd, 2024!',
+        image: 'https://upsurge2k24.s3.ap-south-1.amazonaws.com/events/desiDaredevils.png'
     },
     {
-        title: 'Fifa',
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?",
-        image: '/images/events/fifa.jpg'
+        title: 'Coding Relay',
+        description: 'Get ready for the high-energy CODING RELAY at UPSURGE 2K24! Kick off with a Debugging Quiz then, race against time in the Coding Relay. With a prize pool of ₹1200, bring your laptops and join us on August 24th, 2024, at the CSE Department!',
+        image: 'https://upsurge2k24.s3.ap-south-1.amazonaws.com/events/codingRelay.png'
     },
     {
-        title: 'Smack Down',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?',
-        image: '/images/events/smackdown.jpg'
+        title: 'Coding Casino',
+        description: 'Join us for the electrifying "CODING CASINO" at UPSURGE 2K24! Bet before seeing the question and win if you solve it. With a prize pool of ₹2000, the event is on August 23rd, 2024. Are you ready to claim the title of Coding Casino champion?',
+        image: 'https://upsurge2k24.s3.ap-south-1.amazonaws.com/events/codingCasino.png'
+    },
+    {
+        title: 'FIFA',
+        description: "Join the FIFA FEVER CUP! Face off 1v1 with custom tactics, 6-minute halves, and single elimination. Entry is ₹49. Win ₹1000 for first place and show off your skills!",
+        image: 'https://upsurge2k24.s3.ap-south-1.amazonaws.com/events/fifa.png'
+    },
+    {
+        title: 'WWE Smackdown',
+        description: "Step into WWE's golden age with the Smackdown Showdown Championship! Compete in a Survival Round, Elimination Chamber, and Single Match. Register for ₹49 and vie for ₹1000 or ₹500 in prizes!",
+        image: 'https://upsurge2k24.s3.ap-south-1.amazonaws.com/events/smackdown.png'
     },
 
     {
         title: 'Talents Que',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?',
-        image: '/images/events/talentsque.jpg'
+        description: 'Get ready to dazzle at TALENTESQUE on August 23rd, 2024, at noon! Showcase your talent and compete for Rs 5000/- in prizes. Entry fees: ₹49 (solo), ₹99 (duo), ₹149 (group). Don’t miss out!',
+        image: 'https://upsurge2k24.s3.ap-south-1.amazonaws.com/events/talentsque.png'
     },
     {
-        title: 'Unreconciled Stars',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?',
-        image: '/images/events/image3.jpeg'
-    },
-    {
-        title: 'Dragonspine',
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?",
-        image: '/images/events/image.png'
-    },
-    {
-        title: 'Hands on cloud computing session',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?',
-        image: '/images/events/image.png'
-    },
-
-    {
-        title: 'Lantern Rite',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?',
-        image: '/images/events/image2.jpg'
-    },
-    {
-        title: 'Unreconciled Stars',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?',
-        image: '/images/events/image3.jpeg'
-    },
-    {
-        title: 'Dragonspine',
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis vitae ipsam nam beatae, itaque possimus sapiente ea nemo non explicabo incidunt a, quas accusamus consequatur maiores aliquam ad, illum natus?",
-        image: '/images/events/image.png'
-    },
-
+        title: 'Data Science Seminar',
+        description: "Unlock your future in data science and technology! Join us on August 23rd at 1 PM, where our expert speaker will guide you on how to break into the tech industry and jumpstart your career.",
+        image: 'https://upsurge2k24.s3.ap-south-1.amazonaws.com/events/dataScience.png'
+    }
 ];
 
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['upsurge2k24.s3.ap-south-1.amazonaws.com'],
+      },
+};
 
 export default nextConfig;

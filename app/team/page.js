8897584c3f25a9cab775d08c0e8682c842dc2c87
spectropaws <spectropaws777@gmail.com>
2024-08-16 23:46:42 +0530

    team page

--- app/team/page.js
+++ app/team/page.js
@@ -1,24 +1,3 @@
-"use client";
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import MainContainer from '../components/MainContainer';
-import Cards from './Cards';
-
-export default function TeamsPage() {
-    const router = useRouter();
-
-    useEffect(() => {
-        router.push('/home');
-    }, [router]);
-
-    return (
-        <MainContainer>
-            <div className='w-screen h-screen bg-black text-white flex justify-center items-center'><h1>Revealing Soon...</h1></div>
-        </MainContainer>
-    );
-}
-/*
 import MainContainer from '../components/MainContainer';
 import Cards from './Cards'
 export default function TeamsPage() {
@@ -26,4 +5,3 @@ export default function TeamsPage() {
         <Cards/>
     </MainContainer>;
 }
-*/
